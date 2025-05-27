3dd8db21558ff0347b240f8c73e4d3b114492632
Eder Moreira de Oliveira <82673617+ghostkiller5874@users.noreply.github.com>
2025-05-26 21:45:01 -0300

    Update TaskController.php

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -7,11 +7,7 @@ use Illuminate\Support\Facades\Storage;
 
 // MODELS
 use App\Models\Task;
-<<<<<<< HEAD
-
-=======
 use App\Services\TaskService;
->>>>>>> dev
 
 class TaskController extends Controller
 {
@@ -26,14 +22,7 @@ class TaskController extends Controller
      */
     public function index()
     {
-<<<<<<< HEAD
-        $tasks = $this->task->where('user_id', auth()->id())
-            ->whereNull('deleted_at')
-            ->paginate(10);
-        // $tasks= Task::all();
-=======
         $tasks = $this->taskService->getAllTasks(auth()->id());
->>>>>>> dev
         return response()->json($tasks);
     }
 
@@ -55,37 +44,8 @@ class TaskController extends Controller
     {
 
         $request->validate($this->task->rules(), $this->task->feedback());
-<<<<<<< HEAD
-        if ($request->hasFile("arquivo")) {
-            $file = $request->file('arquivo');
-            $extension = $request->file('arquivo')->getClientOriginalExtension();
-            $folder = $this->extFiles($extension);
-
-            //  dd([
-            //     "arquivo" => $file,
-            //     "extensoes" => $extension,
-            //     "diretorio pos extensoes" => $folder
-            // ]);
-
-            $path = $file->store('arquivos' . $folder, 's3', 'public');
-            $url = Storage::disk('s3')->url($path);
-        }
-
-
-        $description = $this->filtro($request->description);
-        $title = $this->filtro($request->title);
-
-        // dd([$description, $title]);
-        $task = $this->task->create([
-            "user_id" => auth()->id(),
-            "title" => $title,
-            "description" => $description ?? null,
-            "attachment_url" => $url ?? null
-        ]);
-=======
         
         $task = $this->taskService->createTask($request->all(), auth()->id());
->>>>>>> dev
         return response()->json($task, 201);
     }
 
@@ -97,37 +57,11 @@ class TaskController extends Controller
 
         $this->authorizeTask($task);
 
-<<<<<<< HEAD
-        $validated = $request->validate($this->task->rules(), $this->task->feedback());
-
-        if ($request->hasFile('arquivo')) {
-            // Se já tiver um arquivo, exclui
-            if ($task->attachment_url) {
-                $this->deleteFromS3($task->attachment_url);
-            }
-
-            $file = $request->file('arquivo');
-            $extension = $request->file('arquivo')->getClientOriginalExtension();
-            $folder = $this->extFiles($extension);
-
-            $path = $file->store('arquivos' . $folder, 's3', 'public');
-            $url = Storage::disk('s3')->url($path);
-        }
-
-        $task->update([
-            'title' => $this->filtro($validated['title']) ?? $this->filtro($task->title),
-            'description' => $this->filtro($validated['description']) ?? $this->filtro($task->description),
-            'attachment_url' => $url ?? null,
-        ]);
-
-        return response()->json($task, 200);
-=======
         $request->validate($this->task->rules(), $this->task->feedback());
 
         $updated = $this->taskService->updateTask($task, $request->all());
 
         return response()->json($updated, 200);
->>>>>>> dev
     }
 
     /**
@@ -136,12 +70,8 @@ class TaskController extends Controller
     public function destroy(Task $task)
     {
         $this->authorizeTask($task);
-<<<<<<< HEAD
-        $task->delete();
-=======
         // $task->delete();
         $this->taskService->deleteTask($task);
->>>>>>> dev
         return response()->json(['message' => 'Tarefa excluída com sucesso.'], 200);
     }
 
@@ -150,17 +80,7 @@ class TaskController extends Controller
      */
     public function deleted()
     {
-<<<<<<< HEAD
-        $tasks = $this->task->onlyTrashed()
-            ->where('user_id', auth()->id())
-            ->get();
-=======
         $tasks = $this->taskService->getDeletedTasks(auth()->id());
-
-        if ($tasks->isEmpty()) {
-            return response()->json(['message' => 'Nenhuma tarefa deletada encontrada.'], 404);
-        }
->>>>>>> dev
 
         if ($tasks->isEmpty()) {
             return response()->json(['message' => 'Nenhuma tarefa deletada encontrada.'], 404);
@@ -181,36 +101,4 @@ class TaskController extends Controller
         }
     }
 
-<<<<<<< HEAD
-    /**
-     * Retorna o diretorio apropriado para cada tipo de arquivo
-     */
-    private function extFiles($path)
-    {
-        switch (strtolower($path)) {
-            case 'png':
-            case 'jpg':
-            case 'jpeg':
-                return '/images';
-
-            case 'doc':
-            case 'docx':
-            case 'txt':
-                return '/documents';
-
-            case 'pdf':
-                return '/pdf';
-
-            default:
-                return '';
-        }
-    }
-
-    private function filtro($filtro)
-    {
-        $stringNova = filter_var($filtro, FILTER_SANITIZE_SPECIAL_CHARS);
-        return $stringNova;
-    }
-=======
->>>>>>> dev
 }
